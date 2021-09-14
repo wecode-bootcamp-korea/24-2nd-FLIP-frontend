bea05823a0a3c2020ae22c4fa313f08387dc706e
ㅇㅅ <es@iuseong-ui-MacBookPro.local>
2021-09-14 18:33:08 +0900

    EDIT : 리뷰 반영 콘솔로그 삭제 및 env파일 gitignore에추가
      # : 모달창으로 변경 팀원과 상의중
    
    EDIT: env 파일 삭제

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
 const SignIn = () => {
-  return <div>signIn</div>;
+  const history = useHistory();
+  const { Kakao } = window;
+
+  const kakaoLoginClick = () => {
+    Kakao.Auth.login({
+      success: function (success) {
+        fetch('http://10.58.5.148:8000/users/signin?access_token=', {
+          method: 'POST',
+          body: JSON.stringify({
+            access_token: success.access_token,
+          }),
+        })
+          .then(res => res.json())
+          .then(res => {
+            localStorage.setItem('kakao-token', res.TOKEN);
+            if (res.TOKEN) {
+              alert('Flip에 오신걸 환엽합니다!');
+              history.push('/');
+            }
+          });
+      },
+    });
+  };
+
+  return (
+    <Container>
+      <Login>로그인 및 회원가입</Login>
+      <KakaoBtn
+        alt="kakao"
+        src="image/kakao_login_medium_narrow.png"
+        onClick={kakaoLoginClick}
+      />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 20px;
+`;
+
+const Login = styled.h2`
+  //color: ${props => props.theme.grey};
+  font-weight: bold;
+  font-size: 18px;
+  letter-spacing: -0.54px;
+  text-align: center;
+`;
+
+const KakaoBtn = styled.img`
+  cursor: pointer;
+  margin-top: 30px;
+`;
 
 export default SignIn;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -8,6 +8,7 @@ const GlobalStyle = createGlobalStyle`
 }
 a {
     text-decoration: none;
+    color: inherit;
 }
 ${reset}
 `;
