31af8b0a65ac5293a518e9e2b3f591272ea5eaa9
ㅇㅅ <es@iuseong-ui-MacBookPro.local>
2021-09-15 17:45:13 +0900

    ADD: Card 컴포넌트 작업중 -레이아웃 완료 -목데이터 이용 useEffect Fetch API 완료 -Bookmark 아이콘 추가 및 북마크 체크 및 언체크 기능 구현 완료 -북마크 체크 기능 백엔드에게 데이터 전송 기능 구현 중 - 카드컴포넌트 수정 및 백엔드로부터 fetch함수 이용해서 데이터 받아오기 구현

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import CardList from './CardList';
+import styled from 'styled-components';
 
-const Card = () => {
-  return <div>ㅋㅏ드</div>;
-};
+function Card() {
+  const [cards, setCard] = useState([]);
+
+  useEffect(() => {
+    fetch('http://10.58.1.56:8000/products/list/1')
+      .then(res => res.json())
+      .then(res => setCard(res.MESSAGE));
+  }, []);
+
+  return (
+    <Container>
+      {cards.map((card, idx) => {
+        return <CardList key={idx} card={card} />;
+      })}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+`;
 
 export default Card;
