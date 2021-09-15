ec94788f25e04aa3ee5a6fced1adacd799535063
ㅇㅅ <es@iuseong-ui-MacBookPro.local>
2021-09-15 17:45:13 +0900

    ADD: Card 컴포넌트 작업중 -레이아웃 완료 -목데이터 이용 useEffect Fetch API 완료 -Bookmark 아이콘 추가 및 북마크 체크 및 언체크 기능 구현 완료 -북마크 체크 기능 백엔드에게 데이터 전송 기능 구현 중 - 카드컴포넌트 수정 및 백엔드로부터 fetch함수 이용해서 데이터 받아오기 구현

---
+++ src/components/Card/CardList.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import StarImg from './StarImg';
+import Explanation from './Explanation';
+
+export default function Card({ card }) {
+  const [bookMark, setBookmark] = useState(false);
+
+  return (
+    <Wrapper>
+      <ImgContainer>
+        <IconWrapper>
+          <Bookmark onClick={() => setBookmark(!bookMark)}>
+            <i class={`${bookMark ? 'fas' : 'far'} fa-bookmark`} />
+          </Bookmark>
+        </IconWrapper>
+        <ProductImg src={card.image_url} />
+      </ImgContainer>
+
+      <Explanation></Explanation>
+
+      <Desc>
+        <span>{card.title}</span>
+      </Desc>
+
+      <Price>
+        <span>{card.price.toLocaleString()}원</span>
+      </Price>
+
+      <RatingSection>
+        <StarImg alt="rating-star" />
+        <span>{card.rating}</span>
+      </RatingSection>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 180px;
+  margin-right: 15px;
+`;
+
+const ImgContainer = styled.div`
+  box-sizing: inherit;
+  position: relative;
+`;
+
+const ProductImg = styled.img`
+  width: 180px;
+  height: 180px;
+  border-radius: 5px;
+`;
+
+const Desc = styled.div`
+  height: auto;
+  max-height: 36px;
+  margin-top: 6px;
+  font-size: 12px;
+  word-break: keep-all;
+  font-weight: 500;
+  text-overflow: ellipsis;
+  overflow-wrap: break-word;
+`;
+
+const Price = styled.div`
+  height: 14px;
+  margin-top: 12px;
+  line-height: 14px;
+  font-weight: 900;
+`;
+
+const RatingSection = styled.div`
+  display: flex;
+  margin-top: 12px;
+`;
+
+const Bookmark = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 170px;
+  height: 18px;
+  margin-top: 3px;
+  cursor: pointer;
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+`;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Card from '../../components/Card/Card.js';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 };
 
 export default Main;
